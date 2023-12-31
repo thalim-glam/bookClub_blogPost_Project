2d437d6c41c4d03e63f36c5a67825018e46aa725
davidmelendez <Dmelz11@github.com>
2023-12-31 12:49:23 -0800

    added code for cards and text input fields to signup.handlebars and login.handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,4 @@
-odule.exports = {
+module.exports = {
     // the helper method 'format_time' will take in a timestamp and return a string with only the time
     format_time: (date) => {
       // We use the 'toLocaleTimeString()' method to format the dare as D:M:YYYY
