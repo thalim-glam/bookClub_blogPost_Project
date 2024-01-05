3d0f1a6a0f4e22a447b66a208c1f9f9f3e0308f6
Tasneem <thalim.glam@gmail.com>
2024-01-05 00:26:38 -0800

    Added GET method to fetch one user based on id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,22 +5,8 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-
-    //  const dbUserData = await User.findAll({
-    //    include: [
-    //      {
-    //        model: Post,
-    //        attributes: ['name', 'description'],
-    //      },
-    //    ],
-    //  });
-
-    //  const users = dbUserData.map((user) =>
-    //    user.get({ plain: true })
-    //  );
     res.render('homepage')//, {
-    console.log('this is line 22!')  // users,
-    // });
+
   } catch (err) {
     console.log(err);
     res.status(500).json("OH NOOOOO!!!! not again X[");
@@ -42,6 +28,23 @@ router.get('/post/:id', async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+// Get for one user based on id
+// If the user is logged in, they can see the post data
+router.get('/user/:id', async (req, res) => {
+  try {
+
+    const dbUserData = await User.findByPk(
+      req.params.id
+    );
+    const user = dbUserData.get({ plain: true });
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
 
 // login information
 router.get('/login', (req, res) => {
