6714a7e06d5c3937af4df12b531ab4409d86d2ad
savan <savannahwilliams243@gmail.com>
2024-01-04 23:05:27 -0500

    changed format_time to format_date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
     // the helper method 'format_time' will take in a timestamp and return a string with only the time
-    format_time: (date) => {
+    format_date: (date) => {
       // We use the 'toLocaleTimeString()' method to format the dare as D:M:YYYY
       return date.toLocaleDateString();
     },
