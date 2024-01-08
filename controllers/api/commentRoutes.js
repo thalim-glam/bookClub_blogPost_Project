a10ccc3dca5be2c23e616c35c01eb24a51464fe7
savan <savannahwilliams243@gmail.com>
2024-01-08 13:30:23 -0800

    added POST request for creating a comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,3 +1,20 @@
 // imports
 const router = require("express").Router();
 const { Post, Comment, User } = require("../../models");
+
+// CREATE Comment
+router.post("/", async (req, res) => {
+    try {
+      console.log("we made it");
+      const comment = await Comment.create({
+        comment_body: req.body.comment_body,
+        blogPost_id: req.body.blogPost_id,
+        user_id: req.session.user_id || req.body.user_id,
+      });
+  
+      res.status(200).json(comment);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
