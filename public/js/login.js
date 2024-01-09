4b503b300dab7e6302b41161bc742d32554d6a9c
savan <savannahwilliams243@gmail.com>
2024-01-08 18:43:10 -0800

    changing redirection to homepage

--- public/js/login.js
+++ public/js/login.js
@@ -14,8 +14,8 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/homepage');
+        // If successful, redirect the browser to the root/homepage
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
@@ -37,7 +37,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/');
       } else {
         alert(response.statusText);
       }
