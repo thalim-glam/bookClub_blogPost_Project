9f53e178dba61ad4e6caffff156463799b18e809
Tasneem <thalim.glam@gmail.com>
2024-01-16 13:10:37 -0800

    Added Comment table and setup connectivity with the Post and User table. Also exported Comment table

--- models/index.js
+++ models/index.js
@@ -1,15 +1,31 @@
 const User = require('./Users');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
-// set up relationships,
-// recreate tables
+
 User.hasMany(Post, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Post };
+Post.hasMany(Comment, {
+  foreignKey: blogPost_id,
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Users, {
+  foreignKey: user_id,
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: blogPost_id,
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
